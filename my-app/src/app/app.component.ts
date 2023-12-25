1e164953e5e715fc20fa80df9871023246b4a624
Sven J <51181752+SvenJacobsUni@users.noreply.github.com>
2023-12-25 15:55:16 +0100

    Update Datenschutz/Impressum

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -2,15 +2,21 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { WebSocketService } from './web-socket.service';
 import { ModalContainerComponent } from 'angular-bootstrap-md';
 import { Title } from '@angular/platform-browser';
+import {MatDialog} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   @ViewChild('setsModal', { static: true }) setsModal: ModalContainerComponent;
-  constructor( private titleService: Title,private webSocketService: WebSocketService) {}
+  constructor(
+    private titleService: Title,
+    private webSocketService: WebSocketService,
+    public impressum: MatDialog,
+    public datenschutz: MatDialog,
+    ) {}
 
   public setTitle(newTitle: string) {
     this.titleService.setTitle(newTitle);
@@ -21,7 +27,30 @@ export class AppComponent implements OnInit {
     this.webSocketService.listen('temp_user_id').subscribe((data:any)=>{
       window.localStorage.setItem("temp_user_id", data);
     })
+  }
+  openImpressum() {
+    const dialogRef = this.impressum.open(ImpressumComponent);
 
+    dialogRef.afterClosed().subscribe(result => {
+    });
+  }
 
+  openDatenschutz() {
+    const dialogRef = this.datenschutz.open(DatenschutzComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+    });
   }
 }
+
+@Component({
+  selector: 'impressum',
+  templateUrl: 'impressum.html',
+})
+export class ImpressumComponent {}
+
+@Component({
+  selector: 'datenschutz',
+  templateUrl: 'datenschutz.html',
+})
+export class DatenschutzComponent {}
--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,30 +3,31 @@ import { NgModule } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatTableModule} from '@angular/material/table';
+import { MatTableModule } from '@angular/material/table';
 import { MatSortModule } from '@angular/material/sort';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatInputModule} from '@angular/material/input';
-import {AutosizeModule} from 'ngx-autosize';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
-
-
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatInputModule } from '@angular/material/input';
+import { AutosizeModule } from 'ngx-autosize';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDialogModule } from '@angular/material/dialog';
 import { AppComponent } from './app.component';
 import { BlockyModule } from './blockly/blocky.module';
 import { StudentSiteComponent } from './student-site/student-site.component';
 import { DBDiagramComponent } from './db-diagram/db-diagram.component';
-import { TableComponent} from './table/table.component';
+import { TableComponent } from './table/table.component';
 import { DbTextComponent } from './db-text/db-text.component';
-import { ImpressumComponent } from './impressum/impressum.component';
 import { FileUploadComponent } from './file-upload/file-upload.component';
 import { StartComponent } from './start/start.component';
 import { HomeComponent } from './home/home.component';
+import { ImpressumComponent, DatenschutzComponent } from './app.component';
 
 @NgModule({
   declarations: [
@@ -36,10 +37,10 @@ import { HomeComponent } from './home/home.component';
     TableComponent,
     DbTextComponent,
     ImpressumComponent,
+    DatenschutzComponent,
     FileUploadComponent,
     StartComponent,
     HomeComponent,
-
   ],
   imports: [
     BlockyModule,
@@ -54,13 +55,16 @@ import { HomeComponent } from './home/home.component';
     MatFormFieldModule,
     MatTabsModule,
     MatInputModule,
+    MatDividerModule,
     AutosizeModule,
     MatProgressSpinnerModule,
     MatCardModule,
     MatButtonModule,
-    MatButtonToggleModule
+    MatButtonToggleModule,
+    MatToolbarModule,
+    MatDialogModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
